e310e6bbe6b7f9bec303710498fefe63068bd056
otatakahiro <ota7281@gmail.com>
2022-10-26 00:57:38 -0700

    colomn part taka3

--- js/col_validation.js
+++ js/col_validation.js
@@ -1,97 +1,67 @@
-// function colValidation(size,values) {
-//     let colval = [];
-//     let colrow = [];
-//     let check = [];
-
-
-//     for(let j = 0; j < size; j++){
-//         for(let i = 0; i < size; i++){
-//             colrow.push(values[i][j]);
-//         }
-//         colval.push(colrow);
-//         colrow=[];
-//     }
-
-
-//     for (var rowIdx = 0; rowIdx < size; rowIdx++) {
-//         let validRow = [];
-//         let row = colval[rowIdx];
-//         let boolRow = [];
-//         for (var _ = 0; _ < size; _++) {
-//             boolRow.push(false);
-//         }
-//         validRow = findDuplicateNumber(row, boolRow);
-//         check.push(validRow);
-//     }
-
-    
-//     let recolval = check;
-//     let output = [];
-//     let colrow2 = [];
-//     for(let j = 0; j < size; j++){
-//         for(let i = 0; i < size; i++){
-//             colrow2.push(recolval[i][j]);
-//         }
-//         output.push(colrow2);
-//         colrow2=[];
-//     }
-
-
-//     function findDuplicateNumber(row, boolRow) {
-//         for (firstIdx = 0; firstIdx < row.length; firstIdx++) {
-//             for (secondIdx = 0; secondIdx < row.length; secondIdx++) {
-//                 if (row[firstIdx] == row[secondIdx] && firstIdx != secondIdx) {
-//                     boolRow.splice(firstIdx, 1, true);
-//                 }
-//             }
-//         }
-//         return boolRow;
-//     }
-    
-    
-//     return output;
-// }
-
-
-
-    
-
-
-function colValidation(size,values) {
-    let nums = [];
+function colValidation(size, values) {
     let array = [];
-    // let output = [];
-    for(let rowIdx = 0; rowIdx < size; rowIdx++){
-        for(let value = 0; value < size; value++){
-            array.push(values[value][rowIdx]);
-        }  
-        nums.push(array);
-        array = [];
-    }
-
-    
-
+    let row = [];
     let output = [];
-    for (var colIdx = 0; colIdx < nums.length; colIdx++) {
-        let validCol = [];
-        let col = nums;
-        let boolCol = [];
-        for (var _ = 0; _ < col.length; _++) {
-            boolCol.push(false);
-        }
-        validCol = findDuplicateNumber(col, boolCol);
-        output.push(validCol);
-    }
 
-    function findDuplicateNumber(col, boolCol) {
-        for (firstIdx = 0; firstIdx < col.length; firstIdx++) {
-            for (secondIdx = 0; secondIdx < col.length; secondIdx++) {
-                if (col[firstIdx] == col[secondIdx] && firstIdx != secondIdx) {
-                    boolCol.splice(firstIdx, 1, true);
+    // Convert the columns into rows
+    for (let rowIdx = 0; rowIdx < size; rowIdx++) {
+        for (let value = 0; value < size; value++) {
+            row.push(values[value][rowIdx]);
+        }
+        array.push(row);
+        row = [];
+    }
+    
+    // Convert the array to Col
+    function arrayToCol(array){
+        var arrayCol =[];
+        for (let rowIdx = 0; rowIdx < array.length; rowIdx++){
+            for (let value = 0; value < array.length; value++){
+                row.push(values[value][rowIdx]);
+            }
+            arrayCol.push(row);
+            row = [];
+        }return arrayCol;
+    }arrayToCol(array);
+    
+    
+    // Check for duplicates
+    array = rowValidation(size, array);
+    function rowValidation(size, values) {
+        let output = [];
+        
+        for (var rowIdx = 0; rowIdx < size; rowIdx++) {
+            let validRow = [];
+            let row = values[rowIdx];
+            let boolRow = [];
+            for (var _ = 0; _ < size; _++) {
+                boolRow.push(false);
+            }
+            validRow = findDuplicateNumber(row, boolRow);
+            output.push(validRow);
+        }
+        
+        
+        
+        
+
+        function findDuplicateNumber(row, boolRow) {
+            for (firstIdx = 0; firstIdx < row.length; firstIdx++) {
+                for (secondIdx = 0; secondIdx < row.length; secondIdx++) {
+                    if (row[firstIdx] == row[secondIdx] && firstIdx != secondIdx) {
+                        boolRow.splice(firstIdx, 1, true);
+                    }
                 }
             }
+            return boolRow
         }
-        return boolCol;
+
+
+        return output
     }
+
+    output = array;
+
     return output;
 }
+
